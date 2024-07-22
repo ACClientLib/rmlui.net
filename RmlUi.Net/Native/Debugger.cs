7701568d471b6e585a9149d3d834277e0252e364
K J <anticream39@yahoo.com>
2024-07-22 13:43:22 -0400

    add debugger bindings

---
+++ RmlUi.Net/Native/Debugger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RmlUiNet.Native
+{
+    internal static class Debugger
+    {
+        [DllImport("RmlUi.Native", EntryPoint = "rml_Debugger_Initialise")]
+        public static extern void Initialise(IntPtr context);
+
+        [DllImport("RmlUi.Native", EntryPoint = "rml_Debugger_SetContext")]
+        public static extern void SetContext(IntPtr context);
+
+        [DllImport("RmlUi.Native", EntryPoint = "rml_Debugger_Shutdown")]
+        public static extern void Shutdown();
+
+        [DllImport("RmlUi.Native", EntryPoint = "rml_Debugger_IsVisible")]
+        public static extern bool IsVisible();
+
+        [DllImport("RmlUi.Native", EntryPoint = "rml_Debugger_SetVisible")]
+        public static extern void SetVisible(bool visible);
+    }
+}
